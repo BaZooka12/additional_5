9efe0ff2c6545f851cbf9db1d3773a94ee5e49b7
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2018-02-26 12:44:30 +0300

    Brackets: solution v 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = false;
+  if (bracketsConfig.length === 1) {
+    if (bracketsConfig[0][0] === '(') {
+      let regExp = /\(+\)+$/g;
+      if (regExp.test(str)) {
+        result = true;
+      }
+    }
+    if (bracketsConfig[0][0] === '|') {
+      let regExp = /^\|+\|+$/g;
+      if (regExp.test(str)) {
+        result = true;
+      }
+    }
+  }
+  return result;
 }
